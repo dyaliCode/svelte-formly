587c2b1b3a24bf2894c4af903c8b53f67185e464
kamalkech <maroc.develop@gmail.com>
2022-04-02 18:14:46 +0000

    .

--- src/Component.svelte
+++ src/Component.svelte
@@ -23,51 +23,204 @@
   let loading = false;
 
   // Fields
+  // const fields = [
+  //   {
+  //     type: "input",
+  //     name: "x",
+  //     attributes: {
+  //       type: "number",
+  //       classes: ["form-control"],
+  //       label: "X",
+  //     },
+  //     rules: ["required"],
+  //   },
+  //   {
+  //     type: "input",
+  //     name: "y",
+  //     attributes: {
+  //       type: "number",
+  //       classes: ["form-control"],
+  //       label: "Y",
+  //     },
+  //   },
+  //   {
+  //     type: "input",
+  //     name: "total",
+  //     attributes: {
+  //       type: "number",
+  //       classes: ["form-control"],
+  //       label: "X + Y",
+  //     },
+  //     preprocess: (field, fields, values) => {
+  //       if (values.touched === "x" || values.touched === "y") {
+  //         field.value = values.x + values.y;
+  //       }
+  //       return field;
+  //     },
+  //   },
+  //   {
+  //     type: "select",
+  //     name: "city",
+  //     value: 1,
+  //     attributes: {
+  //       classes: ["form-control"],
+  //       label: "City",
+  //     },
+  //     rules: ["required"],
+  //     extra: {
+  //       multiple: true,
+  //       options: [
+  //         {
+  //           value: null,
+  //           title: "None",
+  //         },
+  //         {
+  //           value: 1,
+  //           title: "Agadir",
+  //         },
+  //         {
+  //           value: 2,
+  //           title: "Casablanca",
+  //         },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     type: "select",
+  //     name: "category",
+  //     value: 1,
+  //     attributes: {
+  //       classes: ["form-control"],
+  //       label: "Category",
+  //     },
+  //     rules: ["required"],
+  //     extra: {
+  //       options: [
+  //         {
+  //           value: null,
+  //           title: "None",
+  //         },
+  //         {
+  //           value: 1,
+  //           title: "Users",
+  //         },
+  //         {
+  //           value: 2,
+  //           title: "Posts",
+  //         },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     type: "select",
+  //     name: "items",
+  //     attributes: {
+  //       classes: ["form-control"],
+  //       label: "Items",
+  //     },
+  //     extra: {},
+  //     preprocess: async (field, fields, values) => {
+  //       if (values.touched === "category" || values.category) {
+  //         loading = true;
+  //         field.extra.options =
+  //           values.category == 1 ? await fetchUsers() : await fetchPosts();
+  //         field.value = null;
+  //         loading = false;
+  //       }
+  //       return field;
+  //     },
+  //   },
+  //   {
+  //     type: "checkbox",
+  //     name: "choose",
+  //     attributes: {
+  //       label: "Checkbox",
+  //       id: "choose",
+  //       classes: [""],
+  //     },
+  //     extra: {
+  //       items: [
+  //         {
+  //           name: "item1",
+  //           value: "value1",
+  //           title: "Value 1",
+  //         },
+  //         {
+  //           name: "item2",
+  //           value: "value2",
+  //           title: "Value 2",
+  //         },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     type: "file", // required
+  //     name: "name_file", // require
+  //     attributes: {
+  //       id: "my-custom-field", // optional
+  //       classes: ["form-control"], // optional
+  //       label: "Image", // optional
+  //     },
+  //     extra: {
+  //       multiple: true, // optional
+  //       showPreview: true, // optional
+  //     },
+  //     rules: ["file"],
+  //     file: {
+  //       // need to add this attribute object if you need a file rule
+  //       types: "jpg,gif,png",
+  //       maxsize: 5,
+  //     },
+  //   },
+  //   {
+  //     type: "autocomplete", // required
+  //     name: "name-field-autocomplete", // required
+  //     value: [
+  //       {
+  //         value: 1,
+  //         title: "item 1",
+  //       },
+  //     ],
+  //     attributes: {
+  //       id: "id-field-autocomplete", // required
+  //     },
+  //     extra: {
+  //       multiple: true, // optional
+  //       loadItemes: [
+  //         // list items with id and title attributes.
+  //         {
+  //           value: 1,
+  //           title: "item 1",
+  //         },
+  //         {
+  //           value: 2,
+  //           title: "item 2",
+  //         },
+  //         {
+  //           value: 3,
+  //           title: "item 3",
+  //         },
+  //         {
+  //           value: 4,
+  //           title: "item 4",
+  //         },
+  //       ],
+  //     },
+  //   },
+  // ];
+
   const fields = [
     {
-      type: "input",
-      name: "x",
-      attributes: {
-        type: "number",
-        classes: ["form-control"],
-        label: "X",
-      },
-      rules: ["required"],
-    },
-    {
-      type: "input",
-      name: "y",
-      attributes: {
-        type: "number",
-        classes: ["form-control"],
-        label: "Y",
-      },
-    },
-    {
-      type: "input",
-      name: "total",
-      attributes: {
-        type: "number",
-        classes: ["form-control"],
-        label: "X + Y",
-      },
-      preprocess: (field, fields, values) => {
-        if (values.touched === "x" || values.touched === "y") {
-          field.value = values.x + values.y;
-        }
-        return field;
-      },
-    },
-    {
       type: "select",
-      name: "category",
-      value: 1,
+      name: "city",
+      default_value: [1, 2],
       attributes: {
         classes: ["form-control"],
-        label: "Category",
+        label: "City",
       },
       rules: ["required"],
       extra: {
+        multiple: true,
         options: [
           {
             value: null,
@@ -75,107 +228,11 @@
           },
           {
             value: 1,
-            title: "Users",
+            title: "Agadir",
           },
           {
             value: 2,
-            title: "Posts",
-          },
-        ],
-      },
-    },
-    {
-      type: "select",
-      name: "items",
-      attributes: {
-        classes: ["form-control"],
-        label: "Items",
-      },
-      extra: {},
-      preprocess: async (field, fields, values) => {
-        if (values.touched === "category" || values.category) {
-          loading = true;
-          field.extra.options =
-            values.category == 1 ? await fetchUsers() : await fetchPosts();
-          field.value = null;
-          loading = false;
-        }
-        return field;
-      },
-    },
-    {
-      type: "checkbox",
-      name: "choose",
-      attributes: {
-        label: "Checkbox",
-        id: "choose",
-        classes: [""],
-      },
-      extra: {
-        items: [
-          {
-            name: "item1",
-            value: "value1",
-            title: "Value 1",
-          },
-          {
-            name: "item2",
-            value: "value2",
-            title: "Value 2",
-          },
-        ],
-      },
-    },
-    {
-      type: "file", // required
-      name: "name_file", // require
-      attributes: {
-        id: "my-custom-field", // optional
-        classes: ["form-control"], // optional
-        label: "Image", // optional
-      },
-      extra: {
-        multiple: true, // optional
-        showPreview: true, // optional
-      },
-      rules: ["file"],
-      file: {
-        // need to add this attribute object if you need a file rule
-        types: "jpg,gif,png",
-        maxsize: 5,
-      },
-    },
-    {
-      type: "autocomplete", // required
-      name: "name-field-autocomplete", // required
-      value: [
-        {
-          value: 1,
-          title: "item 1",
-        },
-      ],
-      attributes: {
-        id: "id-field-autocomplete", // required
-      },
-      extra: {
-        multiple: true, // optional
-        loadItemes: [
-          // list items with id and title attributes.
-          {
-            value: 1,
-            title: "item 1",
-          },
-          {
-            value: 2,
-            title: "item 2",
-          },
-          {
-            value: 3,
-            title: "item 3",
-          },
-          {
-            value: 4,
-            title: "item 4",
+            title: "Casablanca",
           },
         ],
       },
--- src/Components/Select.svelte
+++ src/Components/Select.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { createEventDispatcher, afterUpdate } from "svelte";
+  import { createEventDispatcher, onMount, afterUpdate } from "svelte";
   import clsx from "clsx";
 
   import { isRequired } from "../lib/helpers";
@@ -16,19 +16,49 @@
 
   let classe = null;
   let defaulClasses = null;
+  let multiple = false;
 
   // Dispatch.
   const dispatch = createEventDispatcher();
 
   // Change value.
   function onChangeValue(event) {
+    let value;
+    if (multiple) {
+      let values = [];
+      const selectedOptions = event.currentTarget.selectedOptions;
+      for (let i = 0; i < selectedOptions.length; i++) {
+        values.push(selectedOptions[i].value);
+      }
+      value = values;
+    } else {
+      value = event.target.value;
+    }
+
     dispatch("changeValue", {
       name: field.name,
-      value: event.target.value,
+      value,
     });
   }
 
+  function checkSelected(is_multiple, option_value, field_value) {
+    if (is_multiple) {
+      if (field_value && field_value.length) {
+        const res = field_value.indexOf(option_value) != -1;
+        return res;
+      }
+      return null;
+    }
+    return option_value === field_value;
+  }
+
   // Lifecycle.
+  onMount(() => {
+    if (field.extra) {
+      multiple = field.extra.multiple ? field.extra.multiple : null;
+    }
+  });
+
   afterUpdate(() => {
     field.value = field.value == undefined ? null : field.value;
     classe = clsx(field.attributes.classes, defaulClasses);
@@ -41,14 +71,18 @@
   class={classe}
   required={isRequired(field)}
   disabled={field.attributes.disabled}
+  {multiple}
   on:change={onChangeValue}
 >
   {#if field.extra}
     {#if field.extra.options}
       {#each field.extra.options as option}
-        <option value={option.value} selected={option.value === field.value}
-          >{option.title}</option
+        <option
+          value={option.value}
+          selected={checkSelected(multiple, option.value, field.value)}
         >
+          {option.title}
+        </option>
       {/each}
     {/if}
   {/if}
